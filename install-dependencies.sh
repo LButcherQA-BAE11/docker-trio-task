adfe9167d50a840012a5836c80f83738319f6f2d
Ubuntu <ubuntu@ip-172-31-7-26.eu-west-1.compute.internal>
2021-03-26 14:45:59 +0000

    edited images

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -9,7 +9,7 @@ sudo usermod -aG docker jenkins
 
 
 
-# Docker Compost install
+# Docker Compose install
 sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
